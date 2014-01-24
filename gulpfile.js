7534053a0914de8085cb8e05502822e44f45b811
Ryan Benson <benson.ryan@gmail.com>
2014-01-24 10:49:37 -0600

    Cleaned up the main gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,16 @@
 var app, base, coffee, concat, connect, directory, gulp, gutil, hostname, http, lr, open, path, refresh, sass, server, uglify;
 
 gulp = require('gulp');
-
 gutil = require('gulp-util');
-
 coffee = require('gulp-coffee');
-
 concat = require('gulp-concat');
-
 uglify = require('gulp-uglify');
-
 sass = require('gulp-sass');
-
 refresh = require('gulp-livereload');
-
 open = require('gulp-open');
-
 connect = require('connect');
-
 http = require('http');
-
 path = require('path');
-
 lr = require('tiny-lr');
 
 server = lr();
@@ -29,47 +18,55 @@ server = lr();
 gulp.task('webserver', function() {
     var port = 3000;
     hostname = null;
-
     base = path.resolve('app');
-
     directory = path.resolve('app');
-
     app = connect().use(connect["static"](base)).use(connect.directory(directory));
-
     http.createServer(app).listen(port, hostname);
 });
 
 gulp.task('livereload', function() {
-  return server.listen(35729, function(err) {
-    if (err != null) {
-      return console.log(err);
-    }
-  });
+    server.listen(35729, function(err) {
+        if (err != null) {
+            return console.log(err);
+        }
+    });
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('app/scripts/src/**/*.js').pipe(concat('app.js')).pipe(uglify()).pipe(gulp.dest('app/scripts/js')).pipe(refresh(server));
+    gulp.src('app/scripts/src/**/*.js')
+               .pipe(concat('app.js'))
+               .pipe(uglify())
+               .pipe(gulp.dest('app/scripts/js'))
+               .pipe(refresh(server));
 });
 
 gulp.task('styles', function() {
-  return gulp.src('app/styles/scss/init.scss').pipe(sass({
-    includePaths: ['app/styles/scss/general', 'app/styles/scss/pages']
-  })).pipe(concat('styles.css')).pipe(gulp.dest('app/styles/css')).pipe(refresh(server));
+    gulp.src('app/styles/scss/init.scss')
+               .pipe(sass({
+                      includePaths: [
+                          'app/styles/scss/general', 
+                          'app/styles/scss/pages'
+                      ]
+               }))
+               .pipe(concat('styles.css'))
+               .pipe(gulp.dest('app/styles/css'))
+               .pipe(refresh(server));
 });
 
 gulp.task('html', function() {
-  return gulp.src('*.html').pipe(refresh(server));
+    gulp.src('*.html')
+                .pipe(refresh(server));
 });
 
 gulp.task('default', function() {
-  gulp.run('webserver', 'livereload', 'scripts', 'styles');
-  gulp.watch('app/scripts/src/**', function() {
-    return gulp.run('scripts');
-  });
-  gulp.watch('app/styles/scss/**', function() {
-    return gulp.run('styles');
-  });
-  return gulp.watch('app/*.html', function() {
-    return gulp.run('html');
-  });
+    gulp.run('webserver', 'livereload', 'scripts', 'styles');
+    gulp.watch('app/scripts/src/**', function() {
+        return gulp.run('scripts');
+    });
+    gulp.watch('app/styles/scss/**', function() {
+        return gulp.run('styles');
+    });
+    gulp.watch('app/*.html', function() {
+        return gulp.run('html');
+    });
 });
