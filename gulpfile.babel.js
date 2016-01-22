f4e76c5772dc7b0926761c297804979d8028f5cd
Ryan Benson <benson.ryan@gmail.com>
2016-01-22 14:40:52 -0600

    Modularizing tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,44 +1,21 @@
 import gulp from 'gulp';
-import fs from 'fs';
 import prompt from 'gulp-prompt';
-import childProcess from 'child_process';
 import async from 'async';
-import config from './harvest-core/config/config';
 import prompts from './harvest-core/config/prompts';
 
-const exec = childProcess.exec;
+// tasks
+import updatePackage from './harvest-core/tasks/updatePackage';
+import writeCoreConfig from './harvest-core/tasks/writeCoreConfig';
+
 
 gulp.task('configure', (cb) => {
-  const baseDevDependencies = config.baseDevDependencies;
-
   gulp.src('package.json')
       .pipe(prompt.prompt(
         prompts,
         (config) => {
-          config.devDependencies = baseDevDependencies;
-
-          const {name, version, description, main, author, license, devDependencies} = config;
-          const packageJsonData = {name, version, description, main, author, license, devDependencies};
-
           async.parallel([
-            function(cb) {
-              fs.writeFile('package.json', JSON.stringify(packageJsonData, null, 2), (err) => {
-
-                let sudo = '';
-                if(config.sudo === 'Yes') sudo = 'sudo ';
-                exec(sudo + 'npm install; npm prune;', (err, stdout, stderr) => {
-                    if(stdout) console.log(stdout);
-                    if(stderr) console.log(stderr);
-                    cb(err, true);
-                  }
-                );
-              });
-            },
-            function(cb) {
-              fs.writeFile('harvest.json', JSON.stringify(config, null, 2), (err) => {
-                cb(err, true);
-              });
-            }
+            async.apply(updatePackage, config),
+            async.apply(writeCoreConfig, config)
           ],
           function(err, results) {
             console.log('DONE!');
---
+++ harvest-core/tasks/updatePackage.js
@@ -0,0 +1,32 @@
+import async from 'async';
+import fs from 'fs';
+import config from '../config/config';
+import childProcess from 'child_process';
+
+const exec = childProcess.exec;
+
+export default function(config, cb) {
+  const baseDevDependencies = config.baseDevDependencies;
+  config.devDependencies = baseDevDependencies;
+  const {name, version, description, main, author, license, devDependencies} = config;
+  const packageJsonData = {name, version, description, main, author, license, devDependencies};
+
+  async.series([
+    function(cb) {
+      fs.writeFile('package.json', JSON.stringify(packageJsonData, null, 2), (err) => {
+        cb(err, true);
+      });
+    },
+    function(cb) {
+      let sudo = '';
+      if(config.sudo === 'Yes') sudo = 'sudo ';
+      exec(sudo + 'npm install; npm prune;', (err, stdout, stderr) => {
+          if(stdout) console.log(stdout);
+          if(stderr) console.log(stderr);
+          cb(err, true);
+        });
+    }
+  ], function(err, results) {
+    cb(err, results);
+  })
+}
