839eccd234b65542390af0da50134cfdec197d06
Ryan Benson <benson.ryan@gmail.com>
2016-01-22 14:19:48 -0600

    Uses async for task handling, updates core deps

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import fs from 'fs';
 import prompt from 'gulp-prompt';
 import childProcess from 'child_process';
+import async from 'async';
 import config from './harvest-core/config/config';
 import prompts from './harvest-core/config/prompts';
 
@@ -19,18 +20,28 @@ gulp.task('configure', (cb) => {
           const {name, version, description, main, author, license, devDependencies} = config;
           const packageJsonData = {name, version, description, main, author, license, devDependencies};
 
-          fs.writeFile('package.json', JSON.stringify(packageJsonData, null, 2), (err) => {
-            if(err) console.log(err);
-            console.log('Installing NPM modules');
+          async.parallel([
+            function(cb) {
+              fs.writeFile('package.json', JSON.stringify(packageJsonData, null, 2), (err) => {
 
-            let sudo = '';
-            if(config.sudo === 'Yes') sudo = 'sudo ';
-
-            exec(sudo + 'npm install; npm prune;', (err, stdout, stderr) => {
-              if(stdout) console.log(stdout);
-              if(stderr) console.log(stderr);
-              cb(err);
-            });
+                let sudo = '';
+                if(config.sudo === 'Yes') sudo = 'sudo ';
+                exec(sudo + 'npm install; npm prune;', (err, stdout, stderr) => {
+                    if(stdout) console.log(stdout);
+                    if(stderr) console.log(stderr);
+                    cb(err, true);
+                  }
+                );
+              });
+            },
+            function(cb) {
+              fs.writeFile('harvest.json', JSON.stringify(config, null, 2), (err) => {
+                cb(err, true);
+              });
+            }
+          ],
+          function(err, results) {
+            console.log('DONE!');
           });
         }
     ))
--- harvest-core/config/config.js
+++ harvest-core/config/config.js
@@ -1,8 +1,9 @@
 export default {
   baseDevDependencies: {
-    gulp: "^3.9.0",
-    'gulp-prompt': '^0.1.2',
-    'babel-core': '^6.4.5',
-    'babel-preset-es2015': '^6.3.13'
+    "gulp": "^3.9.0",
+    "gulp-prompt": "^0.1.2",
+    "babel-core": "^6.4.5",
+    "babel-preset-es2015": "^6.3.13",
+    "async": "^1.5.2"
   }
 }
