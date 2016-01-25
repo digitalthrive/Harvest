9456778de0df461e1b2523349b7d9f8f880143c8
Ryan Benson <benson.ryan@gmail.com>
2016-01-25 15:49:27 -0600

    Adds documentation to the configure task

--- harvest-core/tasks/configure.js
+++ harvest-core/tasks/configure.js
@@ -1,11 +1,23 @@
+/*
+ * Harvest : Front-End boilerplate
+ * Author: Ryan Benson
+ */
+
+// Core libraries
 import gulp from 'gulp';
 import prompt from 'gulp-prompt';
 import async from 'async';
 import prompts from '../config/prompts';
 
+// Sub tasks
 import updatePackage from './configure/updatePackage';
 import writeCoreConfig from './configure/writeCoreConfig';
 
+/*
+ * default
+ * Public method that updates the NPM package and runs CLI tasks
+ * @params: nil
+ */
 export default function() {
   gulp.src('package.json')
       .pipe(prompt.prompt(
