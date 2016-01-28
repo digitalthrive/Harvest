0e50396f5bb3d2dda7fe61c8ad100732269a8831
Ryan Benson <benson.ryan@gmail.com>
2016-01-28 16:07:03 -0600

    Adds check for a directory  before trying to make it

--- harvest-core/tasks/configure/scaffold.js
+++ harvest-core/tasks/configure/scaffold.js
@@ -1,4 +1,17 @@
+import fs from 'fs';
+
+const isDir = (path, cb) => {
+  fs.stat(path, (err, stats) => {
+    if(err) return cb(err, null);
+    return cb(err, stats.isDirectory());
+  })
+};
+
 export default function(config, cb) {
-  console.log(config);
+  isDir(config.src, (err, results) => {
+    console.log(err);
+    console.log(results);
+  });
+
   cb(null);
 };
